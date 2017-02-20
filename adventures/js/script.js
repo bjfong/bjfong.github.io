ca42d9feaed3075cb346620d8dc5df65d0cc78af
brianfong <brianfong20@yahoo.com>
2017-02-19 22:01:26 -0800

    finished tokyo google maps

--- adventures/js/script.js
+++ adventures/js/script.js
@@ -71,19 +71,151 @@ function initmapOCSD() {
 	var places = [
 		{
 			info: '<font color="black"><strong>The Tipsy Crow</strong><br>\
-					San Diego, CA 92101</font><br>\
+					770 5th Ave<br>San Diego, CA 92101</font><br>\
 					<a href="https://goo.gl/Rj1jLl" target="_blank">Get Directions</a>',
 			lat: centerLat,
 			long: centerLong
 		},
 		{
 			info: '<font color="black"><strong>Whiskey Girl</strong><br>\
-					San Diego, CA 92101</font><br>\
+					702 5th Ave<br>San Diego, CA 92101</font><br>\
 					<a href="https://goo.gl/QzMbTP" target="_blank">Get Directions</a>',
 			lat: 32.7128,
 			long: -117.1603
 		}
 	];
+
+	initMap(places, centerLat, centerLong, zoomFactor);
+}
+
+function initmapTokyo() {
+	var centerLat = 35.700;
+	var centerLong = 139.7454;
+	var zoomFactor = 12;// how close or far to zoom into map 
+
+	var places = [
+		{
+			info: '<font color="black"><strong>Tokyo Tower</strong><br>\
+					4 Chome-2-8 Shibakoen, Minato<br>Tōkyō 105-0011, Japan</font><br>\
+					<a href="https://goo.gl/zKsrkG" target="_blank">Get Directions</a>',
+			lat: 35.6586,
+			long: 139.7454
+		},
+		{
+			info: '<font color="black"><strong>Narita International Airport</strong><br>\
+					1-1 Furugome, Narita-shi<br>Chiba-ken 282-0004, Japan</font><br>\
+					<a href="https://goo.gl/33dmjz" target="_blank">Get Directions</a>',
+			lat: 35.7719867,
+			long: 140.3906561
+		},
+		{
+			info: '<font color="black"><strong>Mt. Fuji</strong><br>\
+					1-1 Furugome, Narita-shi<br>Chiba-ken 282-0004, Japan</font><br>\
+					<a href="https://goo.gl/vWhb4A" target="_blank">Get Directions</a>',
+			lat: 35.3605555,
+			long: 138.7255837
+		},
+		{
+			info: '<font color="black"><strong>Robot Restaurant</strong><br>\
+					歌舞伎町1-7-1 新宿ロボットビル B2F<br>Shinjuku, 東京都 〒160-0013 Japan</font><br>\
+					<a href="https://goo.gl/N7ds1k" target="_blank">Get Directions</a>',
+			lat: 35.6943,
+			long: 139.7028
+		},
+		{
+			info: '<font color="black"><strong>Fukki</strong><br>\
+					芝大門2-1-1<br>Minato, 東京都 〒105-0012 Japan</font><br>\
+					<a href="https://goo.gl/blTVdK" target="_blank">Get Directions</a>',
+			lat: 35.65683,
+			long: 139.75299
+		},
+		{
+			info: '<font color="black"><strong>Hamarikyuonshi Gardens</strong><br>\
+					浜離宮庭園1-1<br>Chūō, 東京都 〒104-0046 Japan</font><br>\
+					<a href="https://goo.gl/6ksGPV" target="_blank">Get Directions</a>',
+			lat: 35.6597,
+			long: 139.7635
+		},
+		{
+			info: '<font color="black"><strong>Tsukiji Fish Market</strong><br>\
+					5 Chome-2-1 Tsukiji<br>Chūō-ku, Tōkyō-to 104-0045, Japan</font><br>\
+					<a href="https://goo.gl/mwpJAi" target="_blank">Get Directions</a>',
+			lat: 35.6655,
+			long: 139.7707
+		},
+		{
+			info: '<font color="black"><strong>Genki Sushi Shibuya</strong><br>\
+					宇田川町24-8 レジャープラザビル 1F<br>Shibuya, 東京都 〒150-0042 Japan</font><br>\
+					<a href="https://goo.gl/xGSXUq" target="_blank">Get Directions</a>',
+			lat: 35.6604,
+			long: 139.6994
+		},
+		{
+			info: '<font color="black"><strong>Hub British Pub</strong><br>\
+					宇田川町22-2 渋谷西村総本店ビル 地下一階<br>Shibuya, 東京都 〒150-0042 Japan</font><br>\
+					<a href="https://goo.gl/Aax82X" target="_blank">Get Directions</a>',
+			lat: 35.6588,
+			long: 139.6990
+		},		
+		{
+			info: '<font color="black"><strong>Shibuya Crossing</strong><br>\
+					道玄坂 渋谷駅ハチ公口前<br>Shibuya, 東京都 〒150-0043 Japan</font><br>\
+					<a href="https://goo.gl/WoYDWM" target="_blank">Get Directions</a>',
+			lat: 35.6618,
+			long: 139.7041
+		},			
+		{
+			info: '<font color="black"><strong>Hachiko Statue</strong><br>\
+					道玄坂 渋谷駅ハチ公口前<br>Shibuya, 東京都 〒150-0043 Japan</font><br>\
+					<a href="https://goo.gl/RAFhQn" target="_blank">Get Directions</a>',
+			lat: 35.6591, 
+			long: 139.7006
+		},	
+		{
+			info: '<font color="black"><strong>Yoyogi Park</strong><br>\
+					代々木神園町2-1<br>Shibuya, 東京都 〒150-0041 Japan</font><br>\
+					<a href="https://goo.gl/2skdF1" target="_blank">Get Directions</a>',
+			lat: 35.6717, 
+			long: 139.6949
+		},		
+		{
+			info: '<font color="black"><strong>Nadaifujisoba Sugamo</strong><br>\
+					巣鴨1-13-4 第67東京ビル<br>Toshima, 東京都 〒170-0002 Japan</font><br>\
+					<a href="https://goo.gl/6LuZbK" target="_blank">Get Directions</a>',
+			lat: 35.7332543, 
+			long: 139.739775
+		},		
+		{
+			info: '<font color="black"><strong>Tsuta Ramen</strong><br>\
+					巣鴨1-14-1 Plateau-Saka 1F<br>Toshima, 東京都 〒170-0002 Japan</font><br>\
+					<a href="https://goo.gl/54nxsF" target="_blank">Get Directions</a>',
+			lat: 35.7329, 
+			long: 139.7406
+		},
+		{
+			info: '<font color="black"><strong>Shinjuku Nakajima</strong><br>\
+					新宿3-32-5 日原ビル Ｂ1Ｆ<br>Shinjuku, 東京都 〒100-0005 Japan</font><br>\
+					<a href="https://goo.gl/jPjeUi" target="_blank">Get Directions</a>',
+			lat: 35.6902, 
+			long: 139.7039
+		},	
+		{
+			info: '<font color="black"><strong>Yoshinoya Akihabara Chūōdōri</strong><br>\
+					外神田1-11-8<br>Chiyoda, 東京都 〒101-0021 Japan</font><br>\
+					<a href="https://goo.gl/jGZZlI" target="_blank">Get Directions</a>',
+			lat: 35.699156, 
+			long: 139.7703
+		},	
+		{
+			info: '<font color="black"><strong>Ameya-Yokochō</strong><br>\
+					上野4丁目<br>Taitō, 東京都 〒110-0005 Japan</font><br>\
+					<a href="https://goo.gl/K5N6m5" target="_blank">Get Directions</a>',
+			lat: 35.7114, 
+			long: 139.7746
+		}		
+	];
+
+	// yoyogi park, uenoyamacho, 
 
 	initMap(places, centerLat, centerLong, zoomFactor);
 }
