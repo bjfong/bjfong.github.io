09de8e0b82fdded67efc78e2cee38466c4840f88
bjfong <bjfong@users.noreply.github.com>
2023-07-03 20:01:08 -0700

    Bfong/init (#18)
    * Initial commit for liver mass calc
    
    * Commented out script and style tags
    
    * Retriggered main branch change
    
    * First algorithm in the accordion
    
    * Added bootstrap and jquery; Added tooltip; Added collapsing accordions
    
    * Added js files
    
    * Removed brs
    
    * Updated body padding; Small css changes
    
    * Fixed bootstrap toggle attributes
    
    * Fixed tooltip display and hide; Removed footer script
    
    * Added data-bs-trigger to fix tooltip
    
    * Added gallbladder calc
    
    * Removed trigger
    
    * Refactored code
    
    * removed merge changes
    
    * Modified script js
    
    * Finished gallbladder calc
    
    * Updated comparators
    
    * Removed debugging, fixed tag
    
    * Fixed clear inputs
    
    * Added LI-RADS calculator
    
    ---------
    
    Co-authored-by: brianfong <brianfong20@yahoo.com>

--- script.js
+++ script.js
@@ -1,3 +1,184 @@
+function liRads_getText(selection) {
+  var textDict = {
+    "hypo_lessThan20_0": "LR3",
+    "hypo_lessThan20_1": "LR3",
+    "hypo_lessThan20_2": "LR4",
+    "hypo_lessThan20_3": "LR4",
+    "hypo_20orMore_0": "LR3",
+    "hypo_20orMore_1": "LR4",
+    "hypo_20orMore_2": "LR4",
+    "hypo_20orMore_3": "LR4",
+    
+    "nonRim_lessThan10_0": "LR3",
+    "nonRim_lessThan10_1": "LR4",
+    "nonRim_lessThan10_2": "LR4",
+    "nonRim_lessThan10_3": "LR4",
+    
+    "nonRim_10to19_0": "LR3",
+    "nonRim_10to19_1": "***",
+    "nonRim_10to19_nonPeripheral": "LR5",
+    "nonRim_10to19_thresholdGrowth": "LR5",
+    "nonRim_10to19_enhancingCapsule": "LR4",
+    "nonRim_10to19_2": "LR5",
+    "nonRim_10to19_3": "LR5",
+    
+    "nonRim_20orMore_0": "LR4",
+    "nonRim_20orMore_1": "LR5",
+    "nonRim_20orMore_2": "LR5",
+    "nonRim_20orMore_3": "LR5",
+  }
+  return textDict[selection] === undefined ? "" : textDict[selection];
+}
+
+function liRads_getAllVisibleInputs(containerClassName, radioButtonClassName) {
+  var elements = document.getElementsByClassName(`${containerClassName}`);
+  var elementsArray = [...elements];
+  var visibleInputs = elementsArray.filter(e => e.style.display === "" || e.style.display === "block");
+
+  var values = [];
+  for (var i = 0; i < visibleInputs.length; i++) {
+    var radioButtons = visibleInputs[i].getElementsByClassName(`${radioButtonClassName}`);
+    for (var j = 0; j < radioButtons.length; j++) {
+      if (radioButtons[j].checked) {
+        values.push(radioButtons[j].value);
+      }
+    }
+  }
+  
+  // is this when it's the ONLY thing selected? or if it's one of the selected?
+  if (values.length === 3 && values.includes("nonRim") && values.includes("10to19") && values.includes("nonPeripheral")) {
+    return "nonRim_10to19_nonPeripheral";
+  }
+  
+  if (values.length === 3 && values.includes("nonRim") && values.includes("10to19") && values.includes("thresholdGrowth")) {
+    return "nonRim_10to19_thresholdGrowth";
+  }
+  
+  if (values.length === 3 && values.includes("nonRim") && values.includes("10to19") && values.includes("enhancingCapsule")) {
+    return "nonRim_10to19_enhancingCapsule";
+  }
+  
+  var numCharacteristicsSelected = values.length - 2;
+  var arr = [values[0], values[1], numCharacteristicsSelected];
+  return arr.join("_");
+}
+function liRads_check(val, name) {
+  if (name === "liRadsArterialPhase" && val === "hypo") {
+    showElement("liRadsHypoSizeDiv", true);
+    
+    showElement("liRadsNonRimSizeDiv", false);
+    showElement("liRadsNonRimCharacteristicsDiv", false);
+    
+    // if this is clicked and size is selected, show characteristics
+    var showHypoCharacteristics = isVisible("liRadsHypoSizeDiv") && (isSelected("liRadsHypoSize-lessThan20") || isSelected("liRadsHypoSize-20orMore"));
+    showElement("liRadsHypoCharacteristicsDiv", showHypoCharacteristics);
+  }
+  
+  if (name === "liRadsArterialPhase" && val === "nonRim") {
+    showElement("liRadsNonRimSizeDiv", true);
+    
+    showElement("liRadsHypoSizeDiv", false);
+    showElement("liRadsHypoCharacteristicsDiv", false);
+    
+    // if this is clicked and size is selected, show characteristics
+    var showNonRimCharacteristics = isVisible("liRadsNonRimSizeDiv") && (isSelected("liRadsNonRimSize-lessThan10") || isSelected("liRadsNonRimSize-10to19") || isSelected("liRadsNonRimSize-20orMore"));
+    showElement("liRadsNonRimCharacteristicsDiv", showNonRimCharacteristics);
+  }
+  
+  if (name === "liRadsHypoSize") {
+    showElement("liRadsHypoCharacteristicsDiv", true);
+  }
+  
+  if (name === "liRadsNonRimSize") {
+    showElement("liRadsNonRimCharacteristicsDiv", true);
+  }
+  
+  var result = "";
+  // if none was selected, it's always going to be one
+  if (val === "none") {
+    if (isVisible("liRadsHypoSizeDiv")) {
+      if (isSelected("liRadsHypoSize-lessThan20")) {
+        result = "hypo_lessThan20_0";
+        clearMultiselect("hypoCharacteristics", "none");
+      }
+      else {
+        result = "hypo_20orMore_0";
+        clearMultiselect("hypoCharacteristics", "none");
+      }
+    }
+    else {
+      if (isSelected("liRadsNonRimSize-lessThan10")) {
+        result = "nonRim_lessThan10_0";
+        clearMultiselect("nonRimCharacteristics", "none");
+      }
+      else if (isSelected("liRadsNonRimSize-10to19")) {
+        result = "nonRim_10to19_0";
+        clearMultiselect("nonRimCharacteristics", "none");
+      }
+      else {
+        result = "nonRim_20orMore_0";
+        clearMultiselect("nonRimCharacteristics", "none");
+      }
+    }
+  }
+  else {
+    // remove "none" from being selected
+    if (isVisible("liRadsHypoSizeDiv")) {
+      setSelected("liRadsHypoCharacteristics-none", false);
+    } else {
+      setSelected("liRadsNonRimCharacteristics-none", false);
+    }
+    
+    // remove "none" from being selected
+    if (isVisible("liRadsNonRimSizeDiv")) {
+      setSelected("liRadsNonRimCharacteristics-none", false);
+    } else {
+      setSelected("liRadsNonRimCharacteristics-none", false);
+    }
+    
+    result = liRads_getAllVisibleInputs('liRadsContainer', 'liRads_radioButton');  
+  }
+  
+  <!--debug(result);-->
+  
+  var textToDisplay = liRads_getText(result);
+  document.getElementById("liRads_answer").value = textToDisplay;
+
+  showCopyTextButton("liRads_answer", "liRads_copyTextId", textToDisplay !== "");
+}
+
+function clearMultiselect(multiSelectClassName, valueToExclude) {
+  var elements = document.getElementsByClassName(`${multiSelectClassName}`);
+  var elementsArray = [...elements];
+  for (var i = 0; i < elementsArray.length; i++) {
+    if (elementsArray[i].value === valueToExclude) {
+      elementsArray[i].selected = true;
+      elementsArray[i].checked = true;
+    } else {
+      elementsArray[i].selected = false;
+      elementsArray[i].checked = false;
+    }
+  }
+}
+
+function liRads_clearInputs() {
+  var elements = document.getElementsByClassName("liRads_radioButton");
+  var elementsArray = [...elements];
+  for (var i = 0; i < elementsArray.length; i++) {
+      elementsArray[i].checked = false;
+  }
+
+  document.getElementById("liRads_answer").value = "";
+
+  showCopyTextButton("liRads_answer", "liRads_copyTextId", false);
+  showElement("liRadsHypoSizeDiv", false);
+  showElement("liRadsNonRimSizeDiv", false);
+  showElement("liRadsHypoCharacteristicsDiv", false);
+  showElement("liRadsNonRimCharacteristicsDiv", false);
+}
+
+// ------------------
+
 function gallbladder_getText(selection) {
   var textDict = {
     "extremelyLow_extremelyLow-lessThan9": "No follow-up",
@@ -106,6 +287,11 @@ function liverMass_getText(selection) {
  */
 function showElement(elementId, show) {
   document.getElementById(`${elementId}`).style.display = show ? 'block' : 'none';
+}
+
+function setSelected(elementId, isSelected) {
+  document.getElementById(`${elementId}`).checked = isSelected;
+  document.getElementById(`${elementId}`).selected = isSelected;
 }
 
 function isVisible(elementId) {
